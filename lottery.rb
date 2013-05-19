f94cc9b33121b30fe830c53c593a1a5113c04044
yu8 <s.tyaro@gmail.com>
2013-05-19 13:10:21 +0900

    リファクタリングその５

--- lottery.rb
+++ lottery.rb
@@ -18,7 +18,22 @@ class Lottery
       hits.push(hitman)
       members.delete(hitman)
     end
-    hits = hits.compact
-    hits
+    hits.compact
   end
 end
+
+lottery = Lottery.new(3)
+lottery.add('John', 1) # 確率  1/20
+lottery.add('Tom',  2) #       2/20
+lottery.add('Bill', 5) #       5/20
+lottery.add('Woz',  2) #       2/20
+lottery.add('Ken', 10) #      10/20
+
+result = Hash.new(0)
+10000.times do
+  lottery.winners.each do |member|
+    result[member] += 1
+  end
+end
+
+puts result
